8423ea7a524d699f57e97a0baf15e5a48f17ed13
David Sevcik <david.sevcik@gmail.com>
2015-11-24 10:38:38 +0000

    Code cleanup

--- lib/reevoo/product.rb
+++ lib/reevoo/product.rb
@@ -1,8 +1,7 @@
 module Reevoo
   class Product < Struct.new(:id, :name)
     def self.all
-      index = 0
-      ['Batmobile', 'Stargate', 'Lightsaber'].map {|name| Product.new(index += 1, name) }
+      ['Batmobile', 'Stargate', 'Lightsaber'].each_with_index.map {|name, i| Product.new(i + 1, name) }
     end
   end
 end
